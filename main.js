b2f0d1730f4ae9eebf714c4913f09fe3473f59ea
RangCloud <mtr008@naver.com>
2023-10-10 13:45:19 +0900

    231010

---
+++ main.js
@@ -0,0 +1,9 @@
+const errorImg = document.getElementById("errorImg");
+
+const errorShow = () => {
+    if (errorImg.style.display == 'none'){
+        errorImg.style.display = 'block';
+    } else {
+
+    }
+}
