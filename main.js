9766731d5496a4ac3160623a2d27c03523357909
RangCloud <mtr008@naver.com>
2023-10-12 14:17:55 +0900

    231012

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 let errorImg = document.getElementById("errorImg");
+let msg = document.getElementById("Msg");
 
 email.addEventListener("input", (e) => {
     mail = e.target.value;
     if(mail.length > 0){
         if (mail.match(/^[.\w_-]+@[\w-]+\.[a-z]{2,4}$/i)){
+            
+        } else {
             errorImg.style.display = 'block';
+            msg.style.display = "block";
         }
     }
 })
