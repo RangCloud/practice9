513c7dc1d8c8460d677a2bb5ff52c41fb35f05b4
RangCloud <mtr008@naver.com>
2023-10-13 14:56:45 +0900

    231013

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ email.addEventListener("input", (e) => {
     mail = e.target.value;
     if(mail.length > 0){
         if (mail.match(/^[.\w_-]+@[\w-]+\.[a-z]{2,4}$/i)){
-            
+            errorImg.style.display = 'none';
+            msg.style.display = 'none';
         } else {
             errorImg.style.display = 'block';
             msg.style.display = "block";
         }
+    } else {
+        errorImg.style.display = 'none';
+            msg.style.display = 'none';
     }
 })
